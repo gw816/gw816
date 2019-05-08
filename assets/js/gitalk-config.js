07ef1b9e38e57567af228407b2e9861c40b0cc46
iGavin <amgavin@163.com>
2019-05-08 21:41:58 +0800

    Update gitalk-config.js

--- assets/js/gitalk-config.js
+++ assets/js/gitalk-config.js
@@ -1,9 +1,9 @@
 var gitalk = new Gitalk({
   	"clientID": "6e0e1ff937d22f9e0b8a",
   	"clientSecret": "266e600c88b22ad8d5d19f5ea4778abf9341b9b8",
-  	"repo": "www.igavin.cc",
-  	"owner": "igavin",
-  	"admin": ["igavin"],
+  	"repo": "gw816.github.io",
+  	"owner": "gw816",
+  	"admin": ["gw816"],
   	"id": window.location.pathname,
   	"distractionFreeMode": false
 	});
